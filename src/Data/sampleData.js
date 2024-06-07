7f56ea01677712674d9180141cf25b1e2c16851f
Ankit Raj <ankitraj7217@gmail.com>
2024-06-07 22:12:19 +0000

    Add Sample Data.

---
+++ src/Data/sampleData.js
@@ -0,0 +1,441 @@
+export const ADVERTISERS_DATA = [
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-01",
+        "impressions": 21365,
+        "clicks": 268,
+        "ctr": "1.25"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-02",
+        "impressions": 19501,
+        "clicks": 382,
+        "ctr": "1.96"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-03",
+        "impressions": 22188,
+        "clicks": 367,
+        "ctr": "1.65"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-04",
+        "impressions": 19720,
+        "clicks": 332,
+        "ctr": "1.68"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-05",
+        "impressions": 18401,
+        "clicks": 253,
+        "ctr": "1.37"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-06",
+        "impressions": 18046,
+        "clicks": 242,
+        "ctr": "1.34"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-07",
+        "impressions": 15839,
+        "clicks": 214,
+        "ctr": "1.35"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-08",
+        "impressions": 14945,
+        "clicks": 196,
+        "ctr": "1.31"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-09",
+        "impressions": 17390,
+        "clicks": 354,
+        "ctr": "2.04"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-10",
+        "impressions": 18086,
+        "clicks": 343,
+        "ctr": "1.9"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-11",
+        "impressions": 24317,
+        "clicks": 413,
+        "ctr": "1.7"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-12",
+        "impressions": 23437,
+        "clicks": 354,
+        "ctr": "1.51"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-13",
+        "impressions": 21449,
+        "clicks": 409,
+        "ctr": "1.91"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-14",
+        "impressions": 18490,
+        "clicks": 364,
+        "ctr": "1.97"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-15",
+        "impressions": 17080,
+        "clicks": 271,
+        "ctr": "1.59"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-16",
+        "impressions": 23184,
+        "clicks": 458,
+        "ctr": "1.98"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-17",
+        "impressions": 22954,
+        "clicks": 345,
+        "ctr": "1.5"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-18",
+        "impressions": 21249,
+        "clicks": 371,
+        "ctr": "1.75"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-19",
+        "impressions": 21240,
+        "clicks": 269,
+        "ctr": "1.27"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-20",
+        "impressions": 19399,
+        "clicks": 309,
+        "ctr": "1.59"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-21",
+        "impressions": 16710,
+        "clicks": 298,
+        "ctr": "1.78"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-22",
+        "impressions": 15505,
+        "clicks": 273,
+        "ctr": "1.76"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-23",
+        "impressions": 19338,
+        "clicks": 511,
+        "ctr": "2.64"
+    },
+    {
+        "advertiser": "Syscox",
+        "date": "2016-02-24",
+        "impressions": 17959,
+        "clicks": 436,
+        "ctr": "2.43"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-01",
+        "impressions": 27115,
+        "clicks": 100,
+        "ctr": "0.37"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-02",
+        "impressions": 28022,
+        "clicks": 154,
+        "ctr": "0.55"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-03",
+        "impressions": 22428,
+        "clicks": 176,
+        "ctr": "0.78"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-04",
+        "impressions": 24401,
+        "clicks": 190,
+        "ctr": "0.78"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-05",
+        "impressions": 23799,
+        "clicks": 121,
+        "ctr": "0.51"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-06",
+        "impressions": 57097,
+        "clicks": 153,
+        "ctr": "0.27"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-07",
+        "impressions": 54290,
+        "clicks": 138,
+        "ctr": "0.25"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-08",
+        "impressions": 38045,
+        "clicks": 122,
+        "ctr": "0.32"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-09",
+        "impressions": 24133,
+        "clicks": 129,
+        "ctr": "0.53"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-10",
+        "impressions": 23378,
+        "clicks": 147,
+        "ctr": "0.63"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-11",
+        "impressions": 27134,
+        "clicks": 156,
+        "ctr": "0.57"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-12",
+        "impressions": 24809,
+        "clicks": 144,
+        "ctr": "0.58"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-13",
+        "impressions": 26000,
+        "clicks": 103,
+        "ctr": "0.4"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-14",
+        "impressions": 24670,
+        "clicks": 134,
+        "ctr": "0.54"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-15",
+        "impressions": 20847,
+        "clicks": 70,
+        "ctr": "0.34"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-16",
+        "impressions": 56739,
+        "clicks": 189,
+        "ctr": "0.33"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-17",
+        "impressions": 45167,
+        "clicks": 129,
+        "ctr": "0.29"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-18",
+        "impressions": 44584,
+        "clicks": 122,
+        "ctr": "0.27"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-19",
+        "impressions": 46565,
+        "clicks": 104,
+        "ctr": "0.22"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-20",
+        "impressions": 44354,
+        "clicks": 105,
+        "ctr": "0.24"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-21",
+        "impressions": 41131,
+        "clicks": 98,
+        "ctr": "0.24"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-22",
+        "impressions": 28840,
+        "clicks": 75,
+        "ctr": "0.26"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-23",
+        "impressions": 17523,
+        "clicks": 110,
+        "ctr": "0.63"
+    },
+    {
+        "advertiser": "Naparos",
+        "date": "2016-02-24",
+        "impressions": 19879,
+        "clicks": 87,
+        "ctr": "0.44"
+    },
+]
+
+export const COUNTRIES_DATA = [
+    {
+        "advertiser": "Syscox",
+        "impressions": 357472,
+        "country": "India"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 78680,
+        "country": "United States"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 33922,
+        "country": "Korea"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 27432,
+        "country": "Germany"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 10460,
+        "country": "Nigeria"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 7609,
+        "country": "Canada"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 3620,
+        "country": "Japan"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 2379,
+        "country": "Spain"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 2323,
+        "country": "Indonesia"
+    },
+    {
+        "advertiser": "Syscox",
+        "impressions": 2197,
+        "country": "Turkey"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 426128,
+        "country": "India"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 142103,
+        "country": "South Korea"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 24714,
+        "country": "United Kingdom"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 18717,
+        "country": "Australia"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 17168,
+        "country": "United States"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 14956,
+        "country": "Russian Federation"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 13292,
+        "country": "Poland"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 11312,
+        "country": "Japan"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 8507,
+        "country": "Nigeria"
+    },
+    {
+        "advertiser": "Naparos",
+        "impressions": 8465,
+        "country": "Ukraine"
+    }
+]
