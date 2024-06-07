8743a79959bc3f6bcc133d0380a4ee14c2b296af
Ankit Raj <ankitraj7217@gmail.com>
2024-06-07 19:24:18 +0000

    Add style to Custom Date.

--- src/Components/CustomDate/index.jsx
+++ src/Components/CustomDate/index.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import "./CustomDate.scss";
 
 const CustomDate = ({
-    name="Date",
-    disabled=false,
-    selectedDate="1971-01-01",
-    setSelectedDate= (date) => {},
-    minDate="1971-01-01",
+  name = "Date",
+  disabled = false,
+  selectedDate = "1971-01-01",
+  setSelectedDate = (date) => {},
+  minDate = "1971-01-01",
 }) => {
-    const _onDateChange = (e) => {
-        setSelectedDate(e?.target?.value);
-    }
+  const _onDateChange = (e) => {
+    setSelectedDate(e?.target?.value);
+  };
 
-    return (
-        <input type="date" name={name} className="custom-date" disabled={disabled}
-            value={selectedDate}
-            onChange={_onDateChange}
-            min={minDate}
-        />
-    )
-}
+  return (
+    <input
+      type="date"
+      name={name}
+      className="custom-date"
+      disabled={disabled}
+      value={selectedDate}
+      onChange={_onDateChange}
+      min={minDate}
+    />
+  );
+};
 
 export default CustomDate;
